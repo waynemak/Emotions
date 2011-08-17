023fc3081180328d077722df769b30a226e4f74a
Wayne Mak <makwayne@gmail.com>
2011-08-16 23:56:17 -0700

    show all emotions on emotions#index as well as ability to edit

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -7,6 +7,15 @@ class EmotionsController < ApplicationController
     @emotion = Emotion.new
   end
 
+  def edit 
+    @emotion = Emotion.find_by_id(params[:id])
+  end
+
+  def update 
+    Emotion.find_by_id(params[:id]).update_attributes(params[:emotion])
+    redirect_to emotions_path
+  end
+
   def create
     @emotion = Emotion.new params[:emotion]
     @emotion.save
--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,2 +1,9 @@
 class Emotion < ActiveRecord::Base
+  def self.avg_feeling 
+    @emotions = find(:all)
+    { 
+      :happy_percent => @emotions.select { |emotion| emotion.feeling == "Happy" }.count.to_f / Emotion.count
+    }
+  end
+
 end
